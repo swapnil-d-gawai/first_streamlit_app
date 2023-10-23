a85929becb46903a40302e179ee7a3849920f0ee
Swapnil Gawai <55853498+swapnil-d-gawai@users.noreply.github.com>
2023-10-23 22:31:53 +0100

    Update streamlit_app.py
    Troubleshooting problem to fix control flow

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -36,6 +37,8 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output on the screen as a table
 streamlit.dataframe(fruityvice_normalized)
 
+#Don't run anythin past here until we troubleshoot
+streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
