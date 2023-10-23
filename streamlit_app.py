893ec6d9f000102c77830ffbf1f24ada269dd195
Swapnil Gawai <55853498+swapnil-d-gawai@users.noreply.github.com>
2023-10-23 19:37:58 +0100

    Update streamlit_app.py
    Added code to call fruity vice api from Streamlit App

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import requests
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -20,3 +21,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
