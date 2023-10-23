ea375b0059c9db4d051e2cc950ab7a670eb2307c
Swapnil Gawai <55853498+swapnil-d-gawai@users.noreply.github.com>
2023-10-23 19:40:13 +0100

    Update streamlit_app.py
    Added new section to display fruityvice api response

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,4 +23,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+
+# New section to display Fruityvice API response
+streamlit.header("Fruityvice Fruit Advice!")
+streamlit.text(fruityvice_response.json())
