3ed7a629735fd3101af9a3f81fabbeed8a102693
Swapnil Gawai <55853498+swapnil-d-gawai@users.noreply.github.com>
2023-10-30 20:53:58 +0000

    Update zenas_streamlit_app.py

--- zenas_streamlit_app.py
+++ zenas_streamlit_app.py
@@ -29,8 +29,7 @@ option = streamlit.selectbox('Pick a sweatsuit color or style:', list(color_list
 product_caption = 'Our warm, comfortable, ' + option + ' sweatsuit!'
 
 # use the option selected to go back and get all the info from the database
-my_cur.execute("select direct_url, price, size_list, upsell_product_desc from catalog_for_website where
-color_or_style = '" + option + "';")
+my_cur.execute("select direct_url, price, size_list, upsell_product_desc from catalog_for_website where color_or_style = '" + option + "';")
 df2 = my_cur.fetchone()
 streamlit.image(
 df2[0],
